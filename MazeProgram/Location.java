b1c22720809fee0bcc11cbb0719a35fb7d8bdca2
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-09-18 09:40:44 -0400

    made location class

---
+++ MazeProgram/Location.java
@@ -0,0 +1,29 @@
+public class Location {
+
+    private int x;
+    private int y;
+
+    /**
+     * @param x the x position
+     * @param y the y position
+     */
+    public Location(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * @return the x position
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * @return the y position
+     */
+    public int getY() {
+        return y;
+    }
+
+}
