57338eda90615bbe32044d1dad21d5a5f5bd1e64
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2020-02-21 10:35:09 -0500

    updated from_git script

--- from_git.bat
+++ from_git.bat
@@ -1,8 +1,9 @@
+@echo off
 git clone https://github.com/rishavb123/%1.git
 cd %1
 rm -f .git
 cd ..
-zip %1
+jar -cfM "%1-Rishav Bhagat.zip" %1
 git add -A
 git commit -m "download %1 from git"
 git push
